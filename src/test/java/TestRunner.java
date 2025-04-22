d0e231006edf038b27f0064a79e93c08d99a35c0
Bharathirajan Damodaran <forbharathi@gmail.com>
2025-04-22 13:16:09 -0400

    Changes with Test Runner

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/features",
-        glue = "stepdefinitions",
+        glue = {"stepdefinitions"},
         plugin = {"pretty", "html:target/cucumber-reports.html"},
         monochrome = true
 )
